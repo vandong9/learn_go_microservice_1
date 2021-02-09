7045017cc9f0db4fa81cb43c8bb5c0c589f6047a
dongha <vandong9@gmail.com>
2021-02-09 23:46:15 +0700

    fix build versel

--- vessel-service/go.mod
+++ vessel-service/go.mod
@@ -5,13 +5,19 @@ go 1.15
 replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
 
 require (
+	github.com/DataDog/zstd v1.4.5 // indirect
 	github.com/golang/protobuf v1.4.3
 	github.com/micro/go-micro v1.18.0 // indirect
 	github.com/micro/go-micro/v2 v2.9.1
 	github.com/micro/protoc-gen-micro/v2 v2.1.1 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.2.1
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
 	golang.org/x/text v0.3.5 // indirect
 	google.golang.org/genproto v0.0.0-20210114201628-6edceaf6022f // indirect
 	google.golang.org/grpc v1.35.0 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+
 )
--- vessel-service/repository.go
+++ vessel-service/repository.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+
 	"gopkg.in/mgo.v2/bson"
 
 	pb "github.com/vandong9/learn_go_microservice_1/vessel-service/proto/vessel"
@@ -21,13 +23,19 @@ type MongoRepository struct {
 	collection *mongo.Collection
 }
 
+/// FindAvailable
 func (repo *MongoRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
 
 	var vessel *pb.Vessel
-	err := repo.collection().Find(bson.M{
-		"capacity":  bson.M{"$gte": spec.Capacity},
-		"maxweight": bson.M{"$gte": spec.MaxWeight},
-	}).One(&vessel)
+	err := repo.collection.FindOne(context.Background(), bson.M{
+		"capacity":  spec.Capacity,
+		"maxweight": spec.MaxWeight,
+	}).Decode(&vessel)
+	// err := repo.collection.FindOne(context.Background(), bson.M{
+	// 	"capacity":  bson.M{"$gte": spec.Capacity},
+	// 	"maxweight": bson.M{"$gte": spec.MaxWeight},
+	// }).Decode(&vessel)
+	//.One(&vessel)
 	if err != nil {
 		return nil, err
 	}
@@ -36,7 +44,8 @@ func (repo *MongoRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, 
 }
 
 func (repository *MongoRepository) Create(vessel *pb.Vessel) error {
-	return repository.collection.Insert(vessel)
+	var _, err = repository.collection.InsertOne(context.Background(), vessel)
+	return err
 }
 
 // func (repo *MongoRepository) Close() {
